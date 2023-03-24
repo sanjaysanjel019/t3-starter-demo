edbc8eb4cabe215f9ab2dac0a30f077f90c201bb
Sanjay Sanjel <sanjay.sanjel018@gmail.com>
2023-03-24 20:50:58 +0545

    added signin/signout button with clerk

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,15 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-
+import { type AppType,AppProps } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
+function MyApp({ Component, pageProps }: AppProps)  {
   return (
-    <SessionProvider session={session}>
+    <ClerkProvider {...pageProps}>
       <Component {...pageProps} />
-    </SessionProvider>
+    </ClerkProvider>
   );
-};
+}
 
 export default api.withTRPC(MyApp);
