77e67dd559e992b15e45289be92427a171f62fe7
Sanjay Sanjel <sanjay.sanjel018@gmail.com>
2023-03-24 21:10:39 +0545

    test'

--- src/env.mjs
+++ src/env.mjs
@@ -8,9 +8,7 @@ const server = z.object({
   DATABASE_URL: z.string().url(),
   NODE_ENV: z.enum(["development", "test", "production"]),
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-  DISCORD_CLIENT_ID: z.string(),
 
-  DISCORD_CLIENT_SECRET: z.string(),
 });
 
 /**
@@ -30,8 +28,7 @@ const client = z.object({
 const processEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
-  DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
-  DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+
 
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
